6fe5253a8e5b922608d00d27b171759e72cc50ef
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-03-14 19:11:10 -0400

    creates students table

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from __init__ import CURSOR, CONN
+
 
 engine = create_engine('sqlite:///my_students.db')
 
@@ -7,13 +9,37 @@ Base = declarative_base()
 
 class Student:
 
+    #__table__name = 'students'
+
     all={}
-    def __init__(self,name, last_name, class_name, class_number, id = None):
+    def __init__(self,name, last_name, period_name, class_number, id = None):
         self.id = id
         self.name = name
         self.last_name = last_name
-        self.class_name = class_name
+        self.period_name = period_name
         self.class_number= class_number
+
+        @classmethod
+        def create_table(cls):
+            """ Create a new table to persist the attributes of Department instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS students (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            last_name TEXT,
+            period_name TEXT,
+            class_number INTEGER)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+        @classmethod
+        def drop_table(cls):
+           """ Drop the table that persists Department instances """
+           sql = """
+               DROP TABLE IF EXISTS departments;
+             """
+        CURSOR.execute(sql)
+        CONN.commit()
 
 class Group:
     all={}
