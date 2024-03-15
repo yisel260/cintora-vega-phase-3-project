093fe844ed5f45fcfd7aa27cc7d1a498542127bb
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-03-14 20:18:51 -0400

    changes topic to books

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,46 +1,37 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from __init__ import CURSOR, CONN
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('sqlite:///my_students.db')
+
+
+engine = create_engine('sqlite:///my_books.db')
 
 Base = declarative_base()
 
-class Student:
+class Books(Base):
 
-    #__table__name = 'students'
+    __table__name = 'books'
 
-    all={}
-    def __init__(self,name, last_name, period_name, class_number, id = None):
-        self.id = id
-        self.name = name
-        self.last_name = last_name
-        self.period_name = period_name
-        self.class_number= class_number
+    id = Column("id",Integer, primary_key=True)
+    name = Column("name", String())
+    author = Column(String())
+    genre= Column(String())
+    rating = Column(Integer)
 
-        @classmethod
-        def create_table(cls):
-            """ Create a new table to persist the attributes of Department instances """
-        sql = """
-            CREATE TABLE IF NOT EXISTS students (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            last_name TEXT,
-            period_name TEXT,
-            class_number INTEGER)
-        """
-        CURSOR.execute(sql)
-        CONN.commit()
-        @classmethod
-        def drop_table(cls):
-           """ Drop the table that persists Department instances """
-           sql = """
-               DROP TABLE IF EXISTS departments;
-             """
-        CURSOR.execute(sql)
-        CONN.commit()
+    if __name__ == '__main__':
+        engine = create_engine('sqlite:///my_books.db')
+        Base.metadata.create_all(engine)
+        
+    def __repr__(self):
+        return f'Id: {self.id} ' \
+        + f'Name: {self.name}'\
+        + f'Author: {self.author}' \
+        + f'Genre: {self.genre} '\
+        + f'Rating: {self.rating}
 
+"""
 class Group:
     all={}
     def __init__ (self,  period, name):
@@ -58,6 +49,7 @@ class Class:
         self.period= period
     def __repr__(self):
         return f"<Class {self.id}: {self.period}>"
+"""
     
     
 
